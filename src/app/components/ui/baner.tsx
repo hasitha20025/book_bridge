487b0c6f6eb302a18f508d009d23c7dd79269b28
hasitha20025 <ict20025@std.uwu.ac.lk>
2024-08-22 21:31:27 +0530

    make baner

---
+++ src/app/components/ui/baner.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Baner(Props:{name:string}) {
+  return (
+    <>
+      <div className="flex items-center justify-center text-sm text-black bg-yellow-200 py-1 mb-2">
+        <p>{Props.name}</p>
+      </div>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Baner from "./components/ui/baner";
 import UserNavbar from "./components/ui/userNavbar";
 
 
 export default function App() {
   return (
     <main className="">
+        <Baner name={"Sri Lanka #1 Book sharing platform"} />
         <UserNavbar/>
     </main>
   );
